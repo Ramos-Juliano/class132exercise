d8c09a083ae8a5e3df29bc2febe8a6e0252dd632
juliano Ramos <ramos-juliano@live.com>
2023-04-01 00:12:09 -0300

    create all classes

---
+++ Aula132Exercicio/Entities/Order.cs
@@ -0,0 +1,25 @@
+﻿using Class132.Entities.Enums;
+using CLass132Exercicio.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Class132Exercicio.Entities
+{
+    class Order
+    {
+        public DateTime Moment { get; set; }
+        public OrderStatus Status { get; set; }
+        public Client CLient { get; set; }
+        public List<OrderItem> Items { get; set; } = new List<OrderItem>();
+
+        public Order()
+        {
+
+        }
+
+        
+    }
+}
